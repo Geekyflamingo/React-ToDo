e3ea73307213ade9098011d46c4ff3f718a9f4f4
Becca <rebecca.vasil@gmail.com>
2016-06-04 22:47:38 -0600

    playing with moment.js and added created at for todos

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,14 +1,22 @@
 var React = require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
 	render: function () {
-		var {text, id, completed} = this.props;
+		var {text, id, completed, createdAt} = this.props;
+		var renderDate = () => {
+			var message = 'Created ';
+			var timestamp = createdAt;
+
+			return message + moment.unix(timestamp).format('MMM Do, YYYY @ h:mm a')
+		};
 		return(
 			<div onClick={() => {
 					this.props.onToggle(id);
 				}}>
 				<input type='checkbox' checked={completed}/>
-				{text}
+				<p>{text}</p>
+				<p>{renderDate()}</p>
 			</div>
 		);
 	}
